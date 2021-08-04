0f845621730c0db6cb52ee01415672c162eab033
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-08-03 22:25:12 -0400

    Subiendo archivos a digitalocean-spaces

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import fileUpload from 'express-fileupload'
 import IndexRoutes from './routes/index.routes'
+import ImageRoutes from './routes/images.routes'
+import config from './config';
 const app = express();
 
 app.set('port', process.env.PORT||4000);
+app.use(fileUpload({
+    tempFileDir:'/temp'
+}));
 app.use(IndexRoutes);
+app.use(ImageRoutes);
 
 app.listen(app.get('port'))
 console.log(`Servidor en puerto: ${app.get('port')}`);
--- server/routes/images.routes.js
+++ server/routes/images.routes.js
@@ -1,10 +1,35 @@
 import { Router } from 'express'
-
+import AWS from 'aws-sdk'
+import config from '../config'
 const router = Router();
+
+const spacesEndpoint = new AWS.Endpoint(config.Endpoint)
+
+const s3 = new AWS.S3({
+    endpoint:spacesEndpoint
+});
 
 router.get('/api/images', async(req,res)=>{})
 router.get('/api/images/:id', async(req,res)=>{})
-router.post('/api/images/upload', async(req,res)=>{})
+router.post('/api/images/upload', async(req,res)=>{
+    const {file} = req.files;
+    try {
+        const uploadObject = await s3.putObject({
+            ACL:'public-read',
+            Bucket: config.BucketName,
+            Body:file.data,
+            Key:file.name
+        }).promise();
+        console.log(uploadObject);
+
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }    
+    return res.json({
+        msg:'recibido'
+    })
+})
 router.delete('/api/images/:id', async(req,res)=>{})
 
 export default router;
