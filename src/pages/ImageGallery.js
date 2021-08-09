6be831a14f09e5b29db3ce8e2450bc6533d86f64
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-08-09 18:30:25 -0400

    Vista para listar las imagenes

--- src/pages/ImageGallery.js
+++ src/pages/ImageGallery.js
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 const ImageGallery = () => {
+    const [images, setImages] = useState([]);
+    useEffect(() => {
+        (async()=>{
+            const res = await axios.get('/api/images');
+            setImages(res.data.images);
+        })();
+    }, []);
     return (
-        <div>
-            Image Gallery
+        <div className="row">
+            {images.map(image=>(
+                <div className="col-md-4 p-1 card-image" key={image._id} onClick={()=>console.log(image._id)}>
+                    <img className="img-fluid h-100 w-100" src={image.url} alt={image.title} />
+                </div>
+            ))}
         </div>
     )
 }
