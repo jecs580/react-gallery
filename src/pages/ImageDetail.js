109734100d3bbdbdb6217575082013b2f7951c2e
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-08-10 23:48:52 -0400

    Vista de Detalle de una imagen

--- src/pages/ImageDetail.js
+++ src/pages/ImageDetail.js
@@ -1,9 +1,36 @@
-import React from 'react'
-
+import axios from 'axios';
+import React,{useState, useEffect} from 'react'
+import { useParams, useHistory } from 'react-router-dom'
 const ImageDetail = () => {
+    const [image, setTmage] = useState({
+        title:'',
+        url:'',
+        _id:'',
+    })
+    const params = useParams();
+    const history = useHistory();
+    useEffect(() => {
+       (async()=>{
+        const res = await axios.get(`/api/images/${params.id}`);
+        setTmage(res.data.image);
+        console.log(res);
+       })();
+    }, [params.id])
+    const handleDelete = async()=>{
+        await axios.delete(`/api/images/${params.id}`);
+        history.push('/')
+    }
     return (
-        <div>
-            ImageDetail
+        <div className="row">
+            <div className="col-md-4 offset-md-4">
+                        <img src={image.url} alt={image.title} className="card-img-top" />
+                <div className="card bg-dark">
+                    <div className="card-body">
+                        <h1>{image.title}</h1>
+                        <button className="btn btn-outline-danger" onClick={handleDelete}>Borrar</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/ImageGallery.js
+++ src/pages/ImageGallery.js
@@ -1,7 +1,9 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 const ImageGallery = () => {
     const [images, setImages] = useState([]);
+    const history =  useHistory();
     useEffect(() => {
         (async()=>{
             const res = await axios.get('/api/images');
@@ -11,7 +13,7 @@ const ImageGallery = () => {
     return (
         <div className="row">
             {images.map(image=>(
-                <div className="col-md-4 p-1 card-image" key={image._id} onClick={()=>console.log(image._id)}>
+                <div className="col-md-4 p-1 card-image" key={image._id} onClick={()=>history.push(`/images/${image._id}`)}>
                     <img className="img-fluid h-100 w-100" src={image.url} alt={image.title} />
                 </div>
             ))}
