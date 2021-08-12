64bd997dbd3cf7ed18afc10a1c1ea931f1b5f349
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-08-11 23:34:47 -0400

    Agregando un loader parar mostrar el progreso de subir imagenes

--- src/pages/ImageForm.js
+++ src/pages/ImageForm.js
@@ -3,19 +3,37 @@ import axios from 'axios'
 const ImageForm = () => {
     const [file, setFile] = useState();
     const [title, setTitle] = useState('')
+    const [uploadPercentage, setUploadPercentage] = useState(0)
+    const [loading, setLoading] = useState(false)
     const handleChange= e=>{
         setFile(e.target.files[0])
     }
     const handleSubmit= async(e)=>{
         e.preventDefault();
+        setLoading(true)
         const formData = new FormData();
         formData.append('file',file);
         formData.append('title',title);
-        const res = await axios.post('/api/images/upload',formData);
+        const res = await axios.post('/api/images/upload',formData,{
+            headers:{
+                'Content-Type':'multipart/form-data'
+            },
+            onDownloadProgress(progressEvent){
+                const {loaded, total} = progressEvent;
+                const percent = parseInt((loaded *100)/total);
+                setUploadPercentage(percent)
+            }
+        });
+        setLoading(false);
         console.log(res);
     }
     return (
         <div className="col-md-4 offset-md-4">
+            {loading&&(
+                <div className="progress rounded-0">
+                    <div className="progress-bar bg-success" role="progressbar" style={{width:`${uploadPercentage}%`}}>{uploadPercentage}</div>
+                </div>
+            )}
             <div className="card bg-dark text-light rounded-0 p-4">
                 <div className="card-body">
                     <h3>Subir una Imagen</h3>
